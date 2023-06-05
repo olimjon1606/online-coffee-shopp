31f913c3c2011b1b2c7bbeb9a82f7c3948797004
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-06-05 12:38:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
+
+}
+function cart() {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
+
 }
 
 let searchForm = document.querySelector('.search-form');
